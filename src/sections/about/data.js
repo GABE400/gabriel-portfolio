3fbc6168312cbe24fd0e72e8d059a2c806a2b14e
GABE400 <gabechipaya@gmail.com>
2023-04-12 11:26:43 +0200

    change made

--- src/sections/about/data.js
+++ src/sections/about/data.js
@@ -1,20 +1,20 @@
-import { FaAward } from "react-icons/fa";
-import { TbSchool } from "react-icons/tb";
-import { BiCodeAlt } from "react-icons/bi";
+import { FaAward } from 'react-icons/fa';
+import { TbSchool } from 'react-icons/tb';
+import { BiCodeAlt } from 'react-icons/bi';
 
 const data = [
-  { id: 1, icon: <TbSchool />, title: "Graduate", desc: "BSc in computing" },
+  { id: 1, icon: <TbSchool />, title: 'Graduate', desc: 'BSc in computing' },
   {
     id: 2,
     icon: <BiCodeAlt />,
-    title: "Full Stack",
-    desc: "Software Developer",
+    title: 'Full Stack',
+    desc: 'Software Developer',
   },
   {
     id: 3,
     icon: <FaAward />,
-    title: "Experience",
-    desc: "01-year working",
+    title: 'Experience',
+    desc: '1 Year',
   },
 ];
 
